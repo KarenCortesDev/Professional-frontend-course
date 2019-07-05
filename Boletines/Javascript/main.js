1724202d0dd6b94bca02bb1a26b3f47f85cc96df
KarenCortesDev <39500881+KarenCortesDev@users.noreply.github.com>
2019-07-04 23:41:34 -0500

    commit
    javascript

---
+++ Boletines/Javascript/main.js
@@ -0,0 +1,10 @@
+console.log("Hola desde un archivo externo de Javascript");
+
+//DOM hacer una busqueda
+//esto recorre el dom buscando un elemento que contenga la
+// clase container, y retorna el elemento encontradp
+let container = document.querySelector(".container");
+let tabla = document.querySelector(".table");
+
+console.log(container);
+console.log(tabla);
